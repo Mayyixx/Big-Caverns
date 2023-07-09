15230f0cdad205df2ad63a7c305877a8bbbbec01
YouLeGamer <125671158+YouLeGamer@users.noreply.github.com>
2023-07-09 16:25:01 +0200

    Create scoreboard.hh

---
+++ code /scoreboard.hh
@@ -0,0 +1,19 @@
+# scoreboard
+
+house_settings{
+
+  scoreboard{
+
+    &6&l--- Big Caverns ---
+    *blank line*
+    &9| &fLevel: &9%stat.player/lvl%
+    &3| &fXP: &3%stat.player/xp%/%stat.player/xpmax%
+    &d| &fHotM Tier: &d%stat.player/hotmlvl%
+    &5| &fHotM XP: &5%stat.player/hotmxp%/%stat.player/hotmxpmax%
+    &e| &fMoney: &e%stat.player/money%
+    *blank line*
+    &6| afMining Power: &6%stat.player/mp%
+    
+  }
+  
+}
